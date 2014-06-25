7ff56a1022b68687068ae0f47035d3f720d0f250
Barikad <joachim@coqblin.net>
2014-06-25 13:09:48 +0200

    first release

---
+++ PSEx2013.ps1
@@ -0,0 +1,5 @@
+﻿
+$RemoteEx2013Session = New-PSSession -ConfigurationName Microsoft.Exchange `
+                                     -ConnectionUri http://vjexch2013/PowerShell/ `
+                                     -Authentication Kerberos -Credential (Get-credential)
+Import-PSSession $RemoteEx2013Session 
