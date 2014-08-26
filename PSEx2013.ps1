c2ec5e16133e462c65aee70db94315c41823b156
Barikad <joachim@coqblin.net>
2014-08-26 23:27:00 +0200

    Delete PSEx2013.ps1

--- PSEx2013.ps1
+++
@@ -1,5 +0,0 @@
-﻿
-$RemoteEx2013Session = New-PSSession -ConfigurationName Microsoft.Exchange `
-                                     -ConnectionUri http://vjexch2013/PowerShell/ `
-                                     -Authentication Kerberos -Credential (Get-credential)
-Import-PSSession $RemoteEx2013Session 
